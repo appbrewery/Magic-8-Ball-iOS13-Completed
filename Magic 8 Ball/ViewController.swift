09db68abef040d798ac067338a489c1ef94cad0b
Angela <8798027+angelabauer@users.noreply.github.com>
2019-09-16 15:30:02 +0100

    Update code from Dicee

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
 
     @IBAction func askButtonPressed(_ sender: UIButton) {
         
-        imageView.image = ballArray[Int.random(in: 0...5)]
+        imageView.image = ballArray[Int.random(in: 0...4)]
     }
 }
 
